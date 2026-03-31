80aa61746e007191d376cc9e32fac5b5b1c43b95
Justin <jlintz@users.noreply.github.com>
2026-03-30 23:09:54 -0400

    not a package name

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ if [[ $OS == "mac" ]]; then
     brew install neovim fzf ripgrep node tree-sitter-cli 2>/dev/null || true
 elif [[ $OS == "linux" ]]; then
     sudo apt update -q
-    sudo apt install -f -y -q neovim fzf ripgrep nodejs npm
+    sudo apt install -f -y -q neovim fzf ripgrep nodejs
 fi
 
 # setup config directory and symlinks
