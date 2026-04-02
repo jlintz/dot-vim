51dc9626a4ee885075f3b10d5f16d8aaec2740ae
Justin <jlintz@users.noreply.github.com>
2026-04-01 23:01:54 -0400

    remove defaults

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,16 +2,6 @@ require('lualine').setup({
   options = {
     theme = 'OceanicNext',
     icons_enabled = true,
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' },
   },
   tabline = {
     lualine_a = { { 'buffers', mode = 2 } },
